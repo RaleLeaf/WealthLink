c4b6c7eb2c2be8195051dd2aaf0765103ae2a92b
RaleLeaf <168341542+RaleLeaf@users.noreply.github.com>
2025-04-16 23:26:27 +0800

    Transaction View Fix

--- app/src/main/java/com/example/wealthlink/TransactionAdapter.java
+++ app/src/main/java/com/example/wealthlink/TransactionAdapter.java
@@ -1,4 +1,64 @@
 package com.example.wealthlink;
 
-public class TransactionAdapter {
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.wealthlink.transactionItemClass;
+
+import java.util.List;
+
+public class TransactionAdapter extends RecyclerView.Adapter<TransactionAdapter.TransactionViewHolder> {
+
+    private List<transactionItemClass> transactionList;
+
+    public TransactionAdapter(List<transactionItemClass> transactionList) {
+        this.transactionList = transactionList;
+    }
+
+    @NonNull
+    @Override
+    public TransactionViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_transaction, parent, false);
+        return new TransactionViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TransactionViewHolder holder, int position) {
+        transactionItemClass item = transactionList.get(position);
+        holder.textTitle.setText(item.getTitle());
+        holder.textDate.setText(item.getDate());
+        holder.textAmount.setText(item.getAmount());
+
+        if (item.isIncome()) {
+            holder.textAmount.setTextColor(Color.parseColor("#00AA00")); // green
+            holder.imageIcon.setImageResource(R.drawable.ic_income); // MAKE THE IMAGE FOR GREEN UP ARROW / PROFIT
+        } else {
+            holder.textAmount.setTextColor(Color.parseColor("#FF0000")); // red
+            holder.imageIcon.setImageResource(R.drawable.ic_expense); // MAKE THE IMAGE FOR RED DOWN ARROW / LOSS
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return transactionList.size();
+    }
+
+    public static class TransactionViewHolder extends RecyclerView.ViewHolder {
+        TextView textTitle, textDate, textAmount;
+        ImageView imageIcon;
+
+        public TransactionViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textTitle = itemView.findViewById(R.id.textTitle);
+            textDate = itemView.findViewById(R.id.textDate);
+            textAmount = itemView.findViewById(R.id.textAmount);
+            imageIcon = itemView.findViewById(R.id.imageIcon);
+        }
+    }
 }
--- app/src/main/java/com/example/wealthlink/TransactionHistory.java
+++ app/src/main/java/com/example/wealthlink/TransactionHistory.java
@@ -8,17 +8,25 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.List;
+
 public class TransactionHistory extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_transaction_history);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerTransactions);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        List<transactionItemClass> list = new ArrayList<>();
+        list.add(new TransactionItem("ABC Investment", "13 Oct 2019", "Php 15,000.00", false));
+        list.add(new TransactionItem("Return Corn Partnership", "10 Oct 2019", "Php 15,000.00", true));
+        list.add(new TransactionItem("Citronella Oil", "10 Oct 2019", "Php 15,000.00", false));
+
+        TransactionAdapter adapter = new TransactionAdapter(list);
+        recyclerView.setAdapter(adapter);
+
     }
 }
