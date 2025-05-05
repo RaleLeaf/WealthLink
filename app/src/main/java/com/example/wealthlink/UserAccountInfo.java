d7b40794ba99b783062a2c5a4e9c92552ad0e503
RaleLeaf <168341542+RaleLeaf@users.noreply.github.com>
2025-05-05 23:15:03 +0800

    Furnish User Account Info

--- app/src/main/java/com/example/wealthlink/UserAccountInfo.java
+++ app/src/main/java/com/example/wealthlink/UserAccountInfo.java
@@ -7,18 +7,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.widget.ImageView;
+import android.view.View;
+
 
 public class UserAccountInfo extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_user_account_info);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        ImageView backBtn = findViewById(R.id.backbtn);
+
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish(); // This returns to the previous activity
+            }
         });
     }
 }
