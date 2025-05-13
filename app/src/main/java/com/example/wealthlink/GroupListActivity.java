11cafc9a42f2b529e13ccaec62b6af47d24f275f
Niles-Rondez <142612332+Niles-Rondez@users.noreply.github.com>
2025-05-13 18:12:49 +0800

    Added Intent to join via invite

--- app/src/main/java/com/example/wealthlink/GroupListActivity.java
+++ app/src/main/java/com/example/wealthlink/GroupListActivity.java
@@ -3,6 +3,7 @@ package com.example.wealthlink;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
@@ -12,7 +13,7 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import com.google.android.material.navigation.NavigationView;
 
 public class GroupListActivity extends BaseActivity {
-
+    Button joinGroup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,6 +29,15 @@ public class GroupListActivity extends BaseActivity {
                 Intent viewAccount = new Intent(GroupListActivity.this, AccountView.class);
                 startActivity(viewAccount);
                 drawerLayout.closeDrawer(GravityCompat.START);
+            }
+        });
+
+        joinGroup = findViewById(R.id.btnJoinViaInvite);
+        joinGroup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(GroupListActivity.this, JoinGroupActivity.class);
+                startActivity(intent);
             }
         });
     }
